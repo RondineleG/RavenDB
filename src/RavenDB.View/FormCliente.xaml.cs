a41bbe7b8939c45343639bb6cee77126c19a9d3f
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-09 08:43:00 -0300

    Configurando formulario principal

---
+++ src/RavenDB.View/FormCliente.xaml.cs
@@ -0,0 +1,49 @@
+﻿using RavenDB.Model;
+using RavenDB.Repository;
+using System.Windows;
+
+namespace RavenDB.View
+{
+    public partial class FormCliente : Window
+    {
+        public Cliente Cliente { get; set; }
+
+        RepositorioDeCliente repositorio;
+
+        public FormCliente()
+        {
+            InitializeComponent();
+            Cliente = new Cliente();
+            this.DataContext = Cliente;
+            repositorio = new RepositorioDeCliente();
+        }
+
+        public FormCliente(Cliente cliente)
+        {
+            InitializeComponent();
+            this.DataContext = cliente;
+            Cliente = cliente;
+            repositorio = new RepositorioDeCliente();
+            if (cliente.Indicador != null)
+            {
+                cmbIndicador.SelectedValue = cliente.Indicador.Id;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cmbIndicador.ItemsSource = repositorio.Liste();
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente = (Cliente)this.DataContext;
+            Cliente.Indicador = (Cliente)cmbIndicador.SelectedItem;
+            if (Cliente.Indicador != null)
+            {
+                Cliente.IndicadorId = ((Cliente)cmbIndicador.SelectedItem).Id;
+            }
+            this.Close();
+        }
+    }
+}
--- src/RavenDB.View/MainWindow.xaml.cs
+++ src/RavenDB.View/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using RavenDB.Model;
+using RavenDB.Repository;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RavenDB.View
 {
@@ -20,9 +11,127 @@ namespace RavenDB.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int QUANTIDADE_POR_PAGINA = 10;
+
+        public string IdDoClienteSalvo { get; set; }
+        public RepositorioDeCliente Repositorio { get; set; }
+        public int PaginaAtual { get; set; } = 1;
+        public int QuantidadeTotalDePaginas { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Repositorio = new RepositorioDeCliente();
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private void btnNovo_Click(object sender, RoutedEventArgs e)
+        {
+            var cliente = ChameOEditorDeCliente(new Cliente());
+            Repositorio.Cadastrar(cliente);
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private void CarregueElementosDoBancoDeDados()
+        {
+            //QueryStatistics estatisticas;
+            //lstClientes.DataContext = Repositorio.Liste(PaginaAtual, QUANTIDADE_POR_PAGINA, out estatisticas);
+            //QuantidadeTotalDePaginas = (int)Math.Ceiling((decimal)estatisticas.TotalResults / (decimal)QUANTIDADE_POR_PAGINA);
+            //txtPaginaAtual.Text = $"Página {PaginaAtual} de {QuantidadeTotalDePaginas}; Total de elmentos: {estatisticas.TotalResults}; Duração da consulta: {estatisticas.DurationInMs} ms";
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstClientes.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um item na lista!");
+                return;
+            }
+
+            var cliente = (Cliente)lstClientes.SelectedItem;
+
+            cliente = Repositorio.Consulte(cliente.Id);
+
+            ChameOEditorDeCliente(cliente);
+
+            Repositorio.Atualizar(cliente);
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private Cliente ChameOEditorDeCliente(Cliente cliente)
+        {
+            var formCliente = new FormCliente(cliente);
+            formCliente.ShowDialog();
+            return formCliente.Cliente;
+        }
+
+        private void btnExcluir_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstClientes.SelectedItem == null)
+            {
+                MessageBox.Show("Selecione um item na lista!");
+                return;
+            }
+
+            var cliente = ((Cliente)(lstClientes.SelectedItem));
+            Repositorio.Remover(cliente.Id);
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private void btnAtualizar_Click(object sender, RoutedEventArgs e)
+        {
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private void txtConsulta_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtConsulta.Text))
+            {
+                CarregueElementosDoBancoDeDados();
+                return;
+            }
+
+            lstClientes.DataContext = Repositorio.ConsultePorTermo(txtConsulta.Text);
+        }
+
+        private void txtIdade_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtIdade.Text))
+            {
+                CarregueElementosDoBancoDeDados();
+                return;
+            }
+
+            int idade;
+            if (int.TryParse(txtIdade.Text, out idade))
+            {
+                lstClientes.DataContext = Repositorio.ConsultaPorIdade(idade);
+            }
+            else
+            {
+                lstClientes.DataContext = new List<Cliente>();
+            }
+        }
+
+        private void btnAnterior_Click(object sender, RoutedEventArgs e)
+        {
+            if (PaginaAtual > 1)
+            {
+                PaginaAtual--;
+            }
+
+            CarregueElementosDoBancoDeDados();
+        }
+
+        private void btnProximo_Click(object sender, RoutedEventArgs e)
+        {
+            if (PaginaAtual < QuantidadeTotalDePaginas)
+            {
+                PaginaAtual++;
+            }
+
+            CarregueElementosDoBancoDeDados();
         }
     }
 }
+
